b786956d9ec849d1a94aaa6d589254c56d7c4d5f
luiz8792br <stelitano508@gmail.com>
2024-10-21 15:00:30 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Landing page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/logomarca.png" type="image/x-icon">
+</head>
+<body>
+     <!-- Inicio do menu topo -->
+    <div id="menu_topo">
+        <img src="img/logomarca.png">
+        <div id="btn_id-mobile">
+            <div class="bntArea">ÁREA DO CLIENTE</div>
+        </div>
+    </div>
+
+ <!-- Inicio da seção 1 -->
+    <div id="secao1">
+        <div id="formulario">
+            <img src="img/logomarca.png" alt="">
+            <h1>Desvendando os Segredos da Escrita Persuasiva</h1> <br>
+            <p>Aprenda a escrever com impacto e conquiste resultados incríveis. 
+                Torne-se um mestre e transforme sua comunicação em vendas!</p>
+                <form action="" method="post">
+                    <input type="text" name="" placeholder="Insira seu primeiro nome" required> <br>
+                    <input type="text" name="" placeholder="Insira seu melhor e-mail" required> <br>
+
+                    <button id="button" type="submit">QUERO ME INSCREVER AGORA!</button>
+                </form>
+
+                <div id="fim_formulario">
+                <img id="img_fim" src="img/calendario2.png">
+                  <p> De <b>02 a 05</b> de MAIO <b>às 20h</b> | Online e Gratuito</p>
+    
+                </div>
+            </div>
+        <img src="img/foto1.png">
+
+
+
+    </div> 
+
+
+     <!-- Inicio da seção 2 -->
+    <div id="secao_02">
+        <div>
+            <img src="img/casa.png" alt="">
+            <p style="margin-top: 1rem; letter-spacing: 0.3rem; margin-bottom: -1.2rem;">Aula 1</p>
+            <h2>Os Fundamentos da Copywriting</h2> <br>
+            <p>Descubra como identificar seu público-alvo, compreender suas necessidades e 
+                transformar essas percepções em mensagens persuasivas, utilizando gatilhos mentais poderosos.</p>
+        </div>
+
+        <div>
+            <img src="img/money.png" alt="">
+            <p style="margin-top: 1rem; letter-spacing: 0.3rem; margin-bottom: -1.2rem;">Aula 2</p>
+            <h2>Técnicas Avançadas de Copywriting</h2> <br>
+            <p>Aprenda a criar headlines impactantes, desenvolver o poder da narrativa e dominar a escrita persuasiva 
+                para diferentes canais de comunicação, como websites, e-mails, anúncios e redes sociais.</p> 
+        </div>
+
+        <div>
+            <img src="img/fire.png" alt="">
+            <p style="margin-top: 1rem; letter-spacing: 0.3rem; margin-bottom: -1.2rem;">Aula 3</p>
+            <h2>Copywriting em Ação: <br> Estudos de caso</h2> <br>
+            <p>Desvendando estratégias por trás de campanhas bem-sucedidas, e obtenha insights
+                 valiosos para aplicar em seus próprios projetos, aprimorando suas táticas eficazes de copywriting.</p>
+        </div>
+
+    </div>
+    <div id="borda"></div>
+
+   <!-- Inicio da seção 3 -->
+    <div id="secao_03">
+            <img id="foto3" src="img/foto2.png" alt="">
+        <br>
+        <div id="verificacao">
+            <h2>Para quem este evento é obrigatório?</h2>
+            <p><img src="img/verificado.png" alt=""> Empreendedores que desejam aumentar suas taxas de conversão e impulsionar suas vendas através de mensagens persuasivas.</p>
+            <p><img src="img/verificado.png" alt=""> Profissionais de marketing que buscam aprimorar suas habilidades de copywriting e criar campanhas mais impactantes e eficazes.</p>
+            <p><img src="img/verificado.png" alt=""> Estudantes e aspirantes a copywriters que desejam mergulhar no mundo da escrita persuasiva e aprender com um especialista renomado.</p>
+            <p><img src="img/verificado.png" alt=""> Proprietários de negócios online que desejam melhorar a comunicação com seu público-alvo e destacar-se em um mercado competitivo.</p>
+            <p><img src="img/verificado.png" alt=""> Freelancers e profissionais autônomos que desejam se destacar em suas áreas de atuação e atrair mais clientes através de suas habilidades de escrita persuasiva.</p>
+        </div>
+    </div>
+
+
+     <!-- Inicio da seção 4 -->
+    <div id="secao_04">
+        <h1>conquiste resultados surpreendentes e eleve sua comunicação a um novo nível. Não deixe essa oportunidade escapar!</h1> <br>
+       
+                  <p> <img id="img_secao04" src="img/calendario.png"> De <b>02 a 05</b> de MAIO <b>às 20h</b> | Online e Gratuito</p>
+
+                  <form action="" method="post">
+                      <input id="inp-text" type="text" name="" placeholder="    Insira seu primeiro nome" required> 
+                      <input type="text" name="" placeholder="    Insira seu melhor e-mail" required>
+                      <button id="button2" type="submit">Enviar</button>
+                    </form>
+    </div>
+
+    <!-- Inicio da seção 5 -->
+    <div id="secao_05">
+        <div id="palavras">
+            <h1>Ana Valéria</h1>
+            <p>Especialista em copywriting com mais de 10 anos de experiência, conhecida por sua habilidade 
+                em criar mensagens persuasivas que geram resultados. Ela já trabalhou com grandes marcas e empreendedores 
+                individuais, ajudando-os a impulsionar suas vendas e comunicações. <br><br>
+
+                Além de sua experiência prática, Ana também é uma professora dedicada, compartilhando seu conhecimento em workshops e palestras em todo o país, inspirando e capacitando os alunos a dominarem a arte da escrita persuasiva.</p>
+            </div>
+            <div id="div_img3">
+                <img id="img_foto3" src="img/foto3.png" alt="">
+            </div>
+            
+        </div>
+
+        <div id="borda"></div>
+
+        <footer>
+            <p>ANA VALÉRIA © TODOS OS DIREITOS RESERVADOS</p>
+            <p>POLÍTICA DE PRIVACIDADE - TERMOS DE USO</p>
+        </footer>
+</body>
+</html>
